741185991dca0c2d8ff88c9d3a25bb8f617b5acd
w91379137 <w913791379137@gmail.com>
2020-01-16 15:40:44 +0800

    新增 get 方法

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,4 +1,4 @@
-import { post } from "./tool/post";
+import { get, post } from "./tool/http";
 
 const debug = require('debug')('app:api.service');
 
@@ -10,7 +10,7 @@ export class ApiService {
 
     async askNewOrder(carName: string) {
 
-        return this.convenientReq('test/SearchUnFinishOrder',
+        return this.convPost('test/SearchUnFinishOrder',
             {
                 carName,
             })
@@ -18,7 +18,7 @@ export class ApiService {
 
     async finishOrder(carName: string, OrderId: number) {
 
-        return this.convenientReq(`test/FinishOrder/${OrderId}`,
+        return this.convPost(`test/FinishOrder/${OrderId}`,
             {
                 carName,
                 OrderId,
@@ -27,14 +27,22 @@ export class ApiService {
 
     // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
     // 共用
-    private async convenientReq(path: string, body: any): Promise<any> {
-        debug(`api path: %o body: %o`, path, body);
-        // console.log(`api path: %o body: %o`, path, body);
+    private async convGet(path: string, body: any): Promise<any> {
+        debug(`get path: %o`, path)
+
+        const res = await get(this.serverUrl + path)
+        .catch(this.handleError)
+
+        return res
+    }
+
+    private async convPost(path: string, body: any): Promise<any> {
+        debug(`post path: %o body: %o`, path, body)
 
         const res = await post(this.serverUrl + path, body)
-            .catch(this.handleError);
+            .catch(this.handleError)
 
-        return res;
+        return res
     }
 
     private handleError(error: any): Promise<any> {
--- src/service/tool/http.ts
+++ src/service/tool/http.ts
@@ -1,12 +1,30 @@
 let request = require('request');
 
+export function get(url) {
+    return new Promise((resolve, reject) => {
+
+        request.get({
+            url: url,
+            json: true,
+        }, (err, httpResponse, body) => {
+            if (err) {
+                //console.error('error:', err);
+                reject(err)
+            } else {
+                //console.error(body);
+                resolve(body)
+            }
+        });
+    })
+}
+
 export function post(url, data) {
     return new Promise((resolve, reject) => {
 
         request.post({
             url: url,
             json: true,
-            form: data
+            form: data,
         }, (err, httpResponse, body) => {
             if (err) {
                 //console.error('error:', err);
